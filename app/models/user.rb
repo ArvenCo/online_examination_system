7864ecd6e8408a7cfe049cdf801baa0c5c0d36c8
Arvene Gerard Eco <109406690+ArvenCo@users.noreply.github.com>
2023-12-07 19:47:18 +0800

    user model update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :exams
     validates :name, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true
     validates :role, presence: true
